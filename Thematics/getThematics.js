727f7cfca17174210617595cab5cad6a57780b91
fpereiraduarte <upsk14009877@iscte-iul.pt>
2023-02-08 12:51:06 +0000

    suggested thematics random

--- Thematics/getThematics.js
+++ Thematics/getThematics.js
@@ -7,9 +7,18 @@ router.get('/', async function (req, res) {
     res.send(thematics);
 });
 
+router.get('/suggested', async function (req, res) {
+    suggestedThematic = await query("SELECT * FROM TEMATICA");
+    let id_thematic = Math.floor(Math.random() * (suggestedThematic.length));
+    console.log(id_thematic);
+    res.send(suggestedThematic[id_thematic]);
+});
+
 router.get('/:id_thematic', async function (req, res) {
     thematic = await query("SELECT * FROM TEMATICA WHERE id = ?", [req.params.id_thematic]);
     res.send(thematic);
 });
+
+
 
 module.exports = router;
