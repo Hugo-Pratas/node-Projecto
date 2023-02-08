4e312adc2d03dbb67ed9f9af659a865f32ee45b9
Hugo de Sousa <hugo.pratasdesousa@gmail.com>
2023-02-08 19:10:16 +0000

    Inicial Commit

--- Videos/getVideos.js
+++ Videos/getVideos.js
@@ -3,35 +3,30 @@ const router = express.Router();
 const {query} = require("../Services/Query_db")
 
 router.get('/', async function (req, res) {
-    page=req.query.page*10
+    page=req.query.page
+    if (!page > 0){
+        page=0
+    }
+    page=page*10
     videos = await query("SELECT * FROM VIDEO ORDER BY data_publicacao DESC LIMIT 10 OFFSET ?",[page])
     videos_send = []
-    for (const video of videos) { //peço desculpa aos deuses do backend mas o drupal ainda assombra o nosso frontEnd por isso isto precisa de ser string =)
-        video_object = {};
-        video_object.thumbnail=""
-        video_object.channel=video.canal.toString()
-        video_object.description=video.descricao
-        video_object.category=video.categoria.toString()
-        tags = await query('SELECT tag_id FROM video_tag WHERE video_id=?',[video.id])
-        tags1=[]
-        for (const tag of tags) { //o front end esta a espera de uma string
-            tags1.push(tag.tag_id)
-        }
-        video_object.tags=tags1.join(", ")
-
-        videos_send.push(video_object)
+    for (const video of videos) {
+        videos_send.push(await videoObject(video))
     }
     res.send(videos_send);
 });
 router.get('/:id_video', async function (req, res) {
-    res.send(await getVideo(req.params.id_video));
+    video = await getVideo(req.params.id_video);
+    video=await videoObject(video[0])
+    res.send([video])
 });
 router.get('/suggested/:tag_id', async function (req, res) {
-    videos_tag=await query("SELECT * FROM video_tag WHERE tag_id=?",[req.params.tag_id])
+    videos_tag=await query("SELECT * FROM video_tag WHERE tag_id=? ORDER BY RAND() LIMIT 10",[req.params.tag_id])
     videos = []
     for (const video of videos_tag) {
-        video1 = await getVideo(video.video_id)
-        videos.push(video1[0])
+        video_db = await getVideo(video.video_id)
+
+        videos.push(await videoObject(video_db[0]))
     }
     res.send(videos);
 });
@@ -47,5 +42,35 @@ function getVideo(id_video){
     return new Promise(async function (resolve, reject) {
         resolve(await query("SELECT * FROM VIDEO WHERE id=?", [id_video]))
     })
+}
+
+async function videoObject(video) {  //peço desculpa aos deuses do backend mas o drupal ainda assombra o nosso frontEnd por isso isto precisa de ser string =)
+    return new Promise(async resolve => {
+        video_object = {};
+        video_object.thumbnail = ""
+        video_object.channel = video.canal.toString()
+        video_object.description = video.descricao
+        video_object.category = video.categoria.toString()
+
+        //tags
+        tags = await query('SELECT tag_id FROM video_tag WHERE video_id=?', [video.id])
+        tags1 = []
+        for (const tag of tags) { //o front end esta a espera de uma string
+            tags1.push(tag.tag_id)
+        }
+        video_object.tags = tags1.join(", ")
+
+        //logo
+        logo = await query('SELECT imagem FROM canal where id=?', [video.canal])
+        video_object.channel_logo = logo[0].imagem
+
+        video_object.date = video.data_publicacao
+        video_object.url = video.yt_thumbnail
+        video_object.id = video.id
+        video_object.name = video.titulo
+        video_object.duration = video.duracao
+        resolve(video_object)
+    })
+
 }
 module.exports = router;
